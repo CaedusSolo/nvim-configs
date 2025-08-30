8fbe3385fdbfbe356cd58cd8ced1bd1609ca1322
Ying Tong <yingtong0106@gmail.com>
2025-08-30 21:35:59 +0800

    feat: Update keymaps and options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "fj", "<Esc>", { noremap = true })
+vim.keymap.set("n", "<leader>t", ":botright split | terminal<CR>", { noremap = true, silent = true })
