58d2dc11156c8d17a2218a302ca590e2b992947c
Ying Tong <yingtong0106@gmail.com>
2025-09-14 13:41:44 +0800

    Update Tab keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,7 @@
 
 vim.keymap.set("i", "fj", "<Esc>", { noremap = true })
 vim.keymap.set("n", "<leader>t", ":botright split | terminal<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>fe", "<cmd>NvimTreeFocus<CR>", { desc = "Focus File Explorer" })-- Toggle File Explorer
+vim.keymap.set("n", "<leader>fe", "<cmd>NvimTreeFocus<CR>", { desc = "Focus File Explorer" }) -- Toggle File Explorer
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
 
 -- Map 'nn' in terminal mode to exit to Normal mode
@@ -18,3 +18,18 @@ vim.keymap.set("n", "<leader>fm", function()
     vim.lsp.buf.format({ async = true })
   end
 end, { desc = "Format file" })
+
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Normal mode: Tab indents, Shift-Tab unindents
+map("n", "<Tab>", ">>", opts)
+map("n", "<S-Tab>", "<<", opts)
+
+-- Visual mode: Tab indents selection, Shift-Tab unindents (keeps selection active)
+map("v", "<Tab>", ">gv", opts)
+map("v", "<S-Tab>", "<gv", opts)
+
+-- Insert mode: Shift-Tab unindents one level
+map("i", "<S-Tab>", "<C-d>", opts)
+
